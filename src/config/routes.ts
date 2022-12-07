ea8c77e4d9d2cbfc45b36f6cc96ea17a336e753a
ttc <1149501789@qq.com>
2022-12-07 21:06:14 +0800

    重构了routes，history代码

--- src/config/routes.ts
+++ src/config/routes.ts
@@ -1,7 +1,23 @@
+import { RouteRecordRaw } from "vue-router";
 import { Bar } from "../views/Bar";
 import { Foo } from "../views/Foo";
+import { First } from "../views/welcome/components/First";
+import { Forth } from "../views/welcome/components/Forth";
+import { Second } from "../views/welcome/components/Second";
+import { Third } from "../views/welcome/components/Third";
+import { Welcome } from "../views/welcome/Welcom";
 
-export const routes = [
-  { path: '/', component: Foo },
-  { path: '/about', component: Bar },
-]
+export const routes: RouteRecordRaw[] = [
+  { path: "/", component: Foo },
+  { path: "/about", component: Bar },
+  {
+    path: "/welcome",
+    component: Welcome,
+    children: [
+      { path: "1", component: First },
+      { path: "2", component: Second },
+      { path: "3", component: Third },
+      { path: "4", component: Forth },
+    ],
+  },
+];
